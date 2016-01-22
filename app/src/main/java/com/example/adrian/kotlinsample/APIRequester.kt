bbfc0f6f696b8c3ffda796c1f61170da331b5200
Adrian Marin <adrian@minube.com>
2016-01-22 13:18:31 +0100

    initial commit 3

--- app/src/main/java/com/example/adrian/kotlinsample/APIRequester.kt
+++ app/src/main/java/com/example/adrian/kotlinsample/APIRequester.kt
@@ -1,6 +1,10 @@
 package com.example.adrian.kotlinsample
 
+import android.R
+import android.app.DownloadManager.Request
+import android.net.Uri
 import com.example.adrian.kotlinsample.models.WeatherResponse
+import java.util.concurrent.Executors
 
 /**
  * Created by adrian on 22/01/16.
@@ -8,4 +12,8 @@ import com.example.adrian.kotlinsample.models.WeatherResponse
 
 fun getWeather() : WeatherResponse {
 
+    val executor = Executors.newScheduledThreadPool(4)
+    async(executor) {
+        Request(Uri.parse("http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=44db6a862fba0b067b1930da0d769e98")).run()
+    }
 }
